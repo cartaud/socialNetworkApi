cf4336e41cd68040d3dc5a06afc36b65263e3c6a
Chadd <chadd_artaud@cox.net>
2022-07-27 15:14:59 -0700

    started developing logic for user routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,37 @@
 const { User } = require('../models/User');
 
+// * `PUT` to update a user by its `_id`
+
+// * `DELETE` to remove user by its `_id`
+
 module.exports = {
     getUsers(req, res) {
         User.find()
         .then((users) => res.json(users))
         .catch((err) => res.status(500).json(err))
     },
+    getSingleUser(req, res) {
+        User.findOne({_id: req.params.userId})
+        .select('-__v')
+        .then((user) => 
+            !user
+              ? res.status(404).json({message: 'No user with that ID'})
+              : res.json(user)
+        )
+        .catch((err) => res.status(500).json(err))
+    },
+    // // example data
+    // {
+    //   "username": "lernantino",
+    //   "email": "lernantino@gmail.com"
+    // }
+    createUser(req, res) {
+        User.create(req.body)
+          .then((dbUserData) => res.json(dbUserData))   
+          .catch((err) => res.status(500).json(err))
+    },
+    // updateUser(req, res) {
+    //     User.update({_id: req.params.userId})
+    // },
     
 }
